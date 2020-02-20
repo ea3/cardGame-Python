632aaf6f39cc8081507247eee020953e6e8d5a99
Emilio Araos <ea3dev@gmail.com>
2020-02-20 02:36:25 -0700

    Player and Hand classes

--- cardsgame.py
+++ cardsgame.py
@@ -1,15 +1,49 @@
+from random import shuffle
+
 SUITE = 'H D S C'.split()
 RANKS = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
+
 
 class Deck:
     def __init__(self):
         print("Creating new ordered deck")
-        self.allcards = [(s,r) for s in SUITE for r in RANKS]
+        self.all_cards = [(s, r) for s in SUITE for r in RANKS]
+
+    def shuffle(self):
+        print("Shuffling Deck")
+        shuffle(self.all_cards)
+
+    def split_in_half(self):
+        return self.all_cards[:26], self.all_cards[26:]
 
 
+class Hand:
+
+    def __init__(self, cards):
+        self.cards = cards
+
+    def __str__(self):
+        return "Contains {} cards".format(len(self.cards))
+
+    def add(self, added_cards):
+        self.cards.extend(added_cards)
+
+    def remove_cards(self):
+        return self.cards.pop()
 
 
+class Player:
 
+    def __init__(self, name, hand):
+        self.name = name
+        self.hand = hand
 
-
+    def play_card(self):
         
+
+
+
+
+
+
+
