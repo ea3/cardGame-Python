4d07bec7221593e84bc3ebd876c7b0ac98929321
Emilio Araos <ea3dev@gmail.com>
2020-02-20 02:43:19 -0700

    remove war and still has cards methods

--- cardsgame.py
+++ cardsgame.py
@@ -39,7 +39,24 @@ class Player:
         self.hand = hand
 
     def play_card(self):
-        
+        drawn_card = self.hand.remove_cards()
+        print("{} has placed: {}".format(self.name, drawn_card))
+        print("\n")
+        return drawn_card
+
+    def remove_war_cards(self):
+        war_cards = []
+        for x in range(3):
+            war_cards.append(self.hand.cards.pop())
+        return war_cards
+
+    def still_has_cards(self):
+        return len(self.hand.cards) != 0
+
+
+
+
+
 
 
 
